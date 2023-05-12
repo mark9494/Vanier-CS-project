a415c81dc14cbfb36860137135ce36bef064ed82
JavaMaster514 <103284433+JavaMaster514@users.noreply.github.com>
2023-05-12 09:42:52 -0400

    Update GraphLoader.java

--- KinematicsPhysicsSimulation/app/src/main/java/edu/vanier/PhysicsSimulation/Pendulum/GraphLoader.java
+++ KinematicsPhysicsSimulation/app/src/main/java/edu/vanier/PhysicsSimulation/Pendulum/GraphLoader.java
@@ -4,6 +4,9 @@
  */
 package edu.vanier.PhysicsSimulation.Pendulum;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -11,5 +14,26 @@ import javafx.stage.Stage;
  * @author antho
  */
 public class GraphLoader extends Stage {
+    public GraphLoader(){
+    try{
+            MakeComponents();
+        }catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
     
+    private void MakeComponents() throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Graph.fxml"));
+        
+        loader.setController(new GraphController());
+        
+        Pane pane = loader.load();
+        
+        Scene sc = new Scene(pane);
+        
+        this.setScene(sc);
+        
+        this.show();
+    } 
 }
